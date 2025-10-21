6237173fa13ac37367b75a610297c5840a470fcd
SamuelUenoUTFPR <samuelueno@alunos.utfpr.edu.br>
2025-10-21 11:44:44 -0300

    Inserção de comentários e pequenas correções
    Inseri os comentários finais na classe main.py com relação as classes, metodos e funções utilizadas e ajustei algumas logicas, como a introdução do makedirs na linha 62 para garantir a existencia do diretorio data/

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+"""
+    Módulo principal, sendo o entrypoint do projeto. Ele não executa nenhuma lógica, só coordena a execução das classes
+    Scraper, Downloader e Extractor, na ordem correta para cumprir os requisitos do desafio.
+    O fluxo de execução é: Configuração do logging para todo o projeto; execução do Scraper para extrair os dados da tabela HTML;
+    execução do Downloader para baixar os boletos de forma assíncrona, execução do Extractor para ler os PDF e extrair as linhas digitáveis e,
+    por fim, salvar o conjunto de dados final e completo em um arquivo JSON.
+"""
+
 import asyncio
 import json
 import os
@@ -15,20 +23,23 @@ logging.basicConfig(
         logging.StreamHandler() # Envia os mesmos logs para o terminal.
     ]
 )
-logging.getLogger("httpx").setLevel(logging.WARNING) # Silenciei o httpx
+logging.getLogger("httpx").setLevel(logging.WARNING) # Silenciei o httpx para manter o log mais limpo.
 logger = logging.getLogger(__name__) # Defini um logger para esse arquivo em especifico, assim como fiz nas outras classes que utilizei o logger.
 URL_ALVO = "https://arth-inacio.github.io/scod_scraping_challenge/"
 PASTA_BOLETOS = "boletos"
 ARQUIVO_JSON = "data/dados.json"
 
 def main():
+    """
+        Função principal que executa o pipeline de scraping em 4 etapas.
+    """
     logger.info("Iniciando o processo de Scraping...")
     logger.info("[Etapa 1/4] Iniciando a extração de dados na tabela...")
     scraper = Scraper(url=URL_ALVO)
     dados_tabela = scraper.run()
 
-    if not dados_tabela:
-        logger.exception("Nenhum dado foi extraido, encerrando o processo.")
+    if not dados_tabela: # Encerra o programa se a etapa mais crucial falhar.
+        logger.error("Nenhum dado foi extraido, encerrando o processo.")
         return
     logger.info(f"Extração concluida, {len(dados_tabela)} registros encontrados")
     logger.info("\n[Etapa 2/4] Iniciando o download dos boletos...")
@@ -38,7 +49,7 @@ def main():
     logger.info("\n[Etapa 3/4] Iniciando extração das linhas digitaveis...")
     extractor = Extractor(pasta_boletos=PASTA_BOLETOS)
     dados_finais = []
-    for dado in dados_tabela:
+    for dado in dados_tabela: # Iteração sobre a lista de dados que o Scraper gerou
         nome_arquivo = os.path.basename(dado.get("boleto_url", ""))
         if nome_arquivo:
             linha_digitavel = extractor.extrair_linha_digitavel(nome_arquivo)
@@ -48,12 +59,13 @@ def main():
         dados_finais.append(dado)
     logger.info("Extração concluida")
     logger.info("\n[Etapa 4/4] Salvando dados finais em JSON...")
+    os.makedirs(os.path.dirname(ARQUIVO_JSON), exist_ok=True) # Adicionei para garantir que o subdiretorio data/ exista.
     try:
-        with open(ARQUIVO_JSON, 'w', encoding='utf-8') as f:
-            json.dump(dados_finais, f, indent=2, ensure_ascii=False)
+        with open(ARQUIVO_JSON, 'w', encoding='utf-8') as f: 
+            json.dump(dados_finais, f, indent=2, ensure_ascii=False) # json.dump escreve a estrutura de dados Python no arquivo e o indent=2 formata o JSON de forma legível.
         logger.info(f"Sucesso, dados salvos em '{ARQUIVO_JSON}'")
     except IOError as e:
         logger.exception(f"Erro ao salvar o arquivo JSON: {e}")
     logger.info("\n--- Processo Finalizado ---")
-if __name__ == "__main__":
+if __name__ == "__main__": # Ponto de entrada padrão do Python, o código so vai ser executado se o script for chamado diretamente.
     main()
